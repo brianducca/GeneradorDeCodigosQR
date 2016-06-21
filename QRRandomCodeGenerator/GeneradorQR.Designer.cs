8c87a181a9a39a4ec13d0e2dc03ad5b4d1bf5bad
brian ducca <brian.ducca@gmail.com>
2016-06-20 23:33:42 -0300

    Initial Commit

---
+++ QRRandomCodeGenerator/GeneradorQR.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace QRCodeGenerator
+{
+    partial class GeneradorQR
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidadQR = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnBuscarCarpeta = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad de QR a Generar";
+            // 
+            // txtCantidadQR
+            // 
+            this.txtCantidadQR.Location = new System.Drawing.Point(146, 22);
+            this.txtCantidadQR.Name = "txtCantidadQR";
+            this.txtCantidadQR.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidadQR.TabIndex = 1;
+            this.txtCantidadQR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cantidad_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Carpeta de destino";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(15, 206);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(477, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnBuscarCarpeta
+            // 
+            this.btnBuscarCarpeta.Location = new System.Drawing.Point(146, 116);
+            this.btnBuscarCarpeta.Name = "btnBuscarCarpeta";
+            this.btnBuscarCarpeta.Size = new System.Drawing.Size(29, 23);
+            this.btnBuscarCarpeta.TabIndex = 11;
+            this.btnBuscarCarpeta.Text = "...";
+            this.btnBuscarCarpeta.UseVisualStyleBackColor = true;
+            this.btnBuscarCarpeta.Click += new System.EventHandler(this.btnBuscarCarpeta_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Carpeta Elegida";
+            // 
+            // lblPath
+            // 
+            this.lblPath.Location = new System.Drawing.Point(143, 165);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(344, 13);
+            this.lblPath.TabIndex = 13;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(400, 116);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 72);
+            this.btnGenerar.TabIndex = 14;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtHasta);
+            this.groupBox1.Controls.Add(this.txtDesde);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtHeight);
+            this.groupBox1.Controls.Add(this.txtWidth);
+            this.groupBox1.Controls.Add(this.btnBuscarCarpeta);
+            this.groupBox1.Controls.Add(this.btnGenerar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblPath);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtCantidadQR);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(5, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 194);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuración General";
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(146, 83);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(100, 20);
+            this.txtHasta.TabIndex = 5;
+            this.txtHasta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cantidad_KeyPress);
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(146, 52);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(100, 20);
+            this.txtDesde.TabIndex = 3;
+            this.txtDesde.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cantidad_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Hasta Número";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Desde Número";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(312, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Alto QR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(312, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ancho QR";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(406, 19);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(69, 20);
+            this.txtHeight.TabIndex = 7;
+            this.txtHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cantidad_KeyPress);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(406, 52);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(69, 20);
+            this.txtWidth.TabIndex = 9;
+            this.txtWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cantidad_KeyPress);
+            // 
+            // GeneradorQR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 241);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "GeneradorQR";
+            this.Text = "Generador de QR";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidadQR;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnBuscarCarpeta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtHasta;
+        private System.Windows.Forms.TextBox txtDesde;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWidth;
+    }
+}
+
---
+++ QRRandomCodeGenerator/GeneradorQR.cs
@@ -0,0 +1,120 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeGenerator
+{
+    public partial class GeneradorQR : Form
+    {
+        private int desde = 1;
+        private int hasta= 99999;
+        private int height=300;
+        private int width=300;
+        private PdfDocument QrCodes;
+        public GeneradorQR()
+        {
+            InitializeComponent();
+            txtHasta.Text = hasta.ToString();
+            txtDesde.Text = desde.ToString();
+            txtHeight.Text = height.ToString();
+            txtWidth.Text = width.ToString();
+        }
+
+        private void btnBuscarCarpeta_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrEmpty(txtCantidadQR.Text) || (string.IsNullOrEmpty(lblPath.Text)) || string.IsNullOrEmpty(txtWidth.Text) || string.IsNullOrEmpty(txtHeight.Text)
+                || string.IsNullOrEmpty(txtHasta.Text) || string.IsNullOrEmpty(txtDesde.Text))
+            {
+                MessageBox.Show("Necesitas completar todos los datos para poder generar los QR", this.Text);
+                return;
+            }
+            if (int.Parse(txtHasta.Text)<= int.Parse(txtDesde.Text))
+            {
+                MessageBox.Show("El valor desde no puede ser mayor al valor hasta", this.Text);
+                return;
+            }
+            if (int.Parse(txtDesde.Text)+ int.Parse(txtCantidadQR.Text)> int.Parse(txtHasta.Text))
+            {
+                MessageBox.Show("Cantidad de codigos qr insuficientes para el desde y hasta especificado", this.Text);
+                return;
+            }
+            progressBar1.Maximum = int.Parse(txtCantidadQR.Text);
+            progressBar1.Step = 1;
+            //CREATE THE .PDF
+            QrCodes= new PdfDocument();
+
+            var result = Enumerable.Range(int.Parse(txtDesde.Text), (int.Parse(txtHasta.Text)- int.Parse(txtDesde.Text))).OrderBy(g => Guid.NewGuid()).Take(int.Parse(txtCantidadQR.Text)).ToArray();
+            for (int i = 0; i < int.Parse(txtCantidadQR.Text); i++)
+            {
+
+                Bitmap b =GenerateQR(int.Parse(txtWidth.Text), int.Parse(txtHeight.Text), result.GetValue(i).ToString());
+                string completePath = lblPath.Text + "\\QRCodeNumber" + result.GetValue(i).ToString() + ".jpg";
+                b.Save(completePath);
+                AddImageToPDF(completePath,result.GetValue(i).ToString());
+                progressBar1.PerformStep();
+            }
+            progressBar1.Value = 0;
+            // Save and start View
+            QrCodes.Save(lblPath.Text + "\\QrCodes.pdf");
+            MessageBox.Show("Se completo la operación con éxito",this.Text);
+            Process.Start(lblPath.Text + "\\QrCodes.pdf");
+        }
+
+        private void cantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        public Bitmap GenerateQR(int width, int height, string text)
+        {
+            var bw = new ZXing.BarcodeWriter();
+            var encOptions = new ZXing.Common.EncodingOptions() { Width = width, Height = height, Margin = 0 };
+            bw.Options = encOptions;
+            bw.Format = ZXing.BarcodeFormat.QR_CODE;
+            var result = new Bitmap(bw.Write(text));
+            return result;
+        }
+
+        private void AddImageToPDF(string imageLoc,string qrNumber)
+        {
+            // Create an empty page 
+            PdfPage page = QrCodes.AddPage();
+            page.Size = PdfSharp.PageSize.A4;
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            DrawImage(gfx, imageLoc, 50, 50,qrNumber);
+        }
+
+        void DrawImage(XGraphics gfx, string jpegSamplePath, int x, int y,string qrNumber)
+        {
+            XImage image = XImage.FromFile(jpegSamplePath);
+            gfx.DrawImage(image, x+100,y+175, 300,300);
+            XFont font = new XFont("Arial", 30, XFontStyle.Regular);
+            gfx.DrawString(qrNumber, font, XBrushes.Black,x,y, XStringFormats.Center);
+            
+        }
+    }
+}
